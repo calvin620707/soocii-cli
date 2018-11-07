4cd7e3b67fdc022c833acb9b83d9ba62e4743341
Calvin Wu <calvin620707@msn.com>
2018-11-07 15:40:01 +0800

    try to fix entry point issue

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/calvin620707/soocii-cli'
 EMAIL = 'calvin.wu@soocii.me'
 AUTHOR = 'Calvin Wu'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -104,7 +104,7 @@ setup(
     url=URL,
     packages=find_packages(exclude=('tests',)),
     entry_points={
-        'console_scripts': ['soocii=cli:main'],
+        'console_scripts': ['soocii=app.cli:main'],
     },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
